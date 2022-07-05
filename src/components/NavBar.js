fd2843e54bba35f7b3d4950e6b2d3919d1aa0df9
MithosMan <99754781+MithosMan@users.noreply.github.com>
2022-07-04 21:00:40 -0300

    Add files via upload

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,14 @@
 const Navegacion = () => {
   return (
 <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
-      <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+      <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">ATLAS MITOLOGICO
       </a>
 
       <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-        <li><a href="#" class="nav-link px-2 link-secondary">Home</a></li>
-        <li><a href="#" class="nav-link px-2 link-dark">Features</a></li>
-        <li><a href="#" class="nav-link px-2 link-dark">Pricing</a></li>
-        <li><a href="#" class="nav-link px-2 link-dark">FAQs</a></li>
-        <li><a href="#" class="nav-link px-2 link-dark">About</a></li>
+        <li><a href="#" class="nav-link px-2 link-secondary">Bestiario</a></li>
+        <li><a href="#" class="nav-link px-2 link-dark">Historias</a></li>
+        <li><a href="#" class="nav-link px-2 link-dark">Artefactos</a></li>
+        <li><a href="#" class="nav-link px-2 link-dark">Contacto</a></li>
       </ul>
 
       <div class="col-md-3 text-end">
