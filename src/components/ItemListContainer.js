2d02dc953d2b041134bf12bd9f437f90d939490b
MithosMan <99754781+MithosMan@users.noreply.github.com>
2022-07-13 20:16:29 -0300

    Add files via upload

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,5 +1,20 @@
+import React from 'react';
+import Conteo from './ItemCount';
+
+let {propName: nombre}={propName: "Futuro listado de criaturas fantasticas"}; 
+
 function ListContainer (){
-    <div>Texto provisional</div>
+
+  const onAdd = (valor) => {
+    console.log("Compraste "+valor+" unidades")
+  }
+
+  return (
+    <p>Listado provisional:
+      <div>{nombre}</div> 
+      <Conteo initial={1} stock={5} onAdd={onAdd}/>
+    </p>
+    );
   }
 
   export default ListContainer
